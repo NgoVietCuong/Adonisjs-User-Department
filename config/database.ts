00aa6aacae8370257cd6fc57fe4113d04a20cf2b
ngo cuong <ngocuong972000@gmail.com>
2024-08-11 23:26:35 +0700

    feature: dockerize application

--- config/database.ts
+++ config/database.ts
@@ -2,13 +2,13 @@ import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
 
 const dbConfig = defineConfig({
-  connection: 'postgres',
+  connection: 'mysql',
   connections: {
-    postgres: {
-      client: 'pg',
+    mysql: {
+      client: 'mysql2',
       connection: {
         host: env.get('DB_HOST'),
-        port: Number(env.get('DB_PORT')),
+        port: env.get('DB_PORT'),
         user: env.get('DB_USER'),
         password: env.get('DB_PASSWORD'),
         database: env.get('DB_DATABASE'),
